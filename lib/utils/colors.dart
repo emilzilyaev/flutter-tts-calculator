e0712b6184e427892cf066ff21aa8fb64d158f9a
santoni98 <albertgateev1@mail.ru>
2023-05-14 18:50:06 +0300

    last not least

---
+++ lib/utils/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+/// Dark Colors
+class DarkColors {
+  static const scaffoldBgColor = Color(0xff22252D);
+  static const sheetBgColor = Color(0xff292D36);
+  static const btnBgColor = Color.fromARGB(255, 33, 36, 42);
+  static const leftOperatorColor = Color.fromARGB(255, 112, 193, 179);
+}
+
+/// Light Colors
+class LightColors {
+  static const scaffoldBgColor = Color(0xffFFFFFF);
+  static const sheetBgColor = Color(0xffF9F9F9);
+  static const btnBgColor = Color.fromARGB(255, 243, 243, 243);
+  static const operatorColor = Color.fromARGB(255, 242, 95, 92);
+  static const leftOperatorColor = Color.fromARGB(255, 112, 193, 179);
+}
---
+++ lib/utils/flutter_tts.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_tts/flutter_tts.dart';
+
+void speak(String input) {
+  FlutterTts flutterTts = FlutterTts();
+  flutterTts.speak(input);
+  // Speak
+}
